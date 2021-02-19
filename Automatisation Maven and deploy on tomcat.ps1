47b26fae3dacfcda097bb87356e2b00b22ca9551
flav1-ann <Moitoi28>
2021-02-19 14:36:52 +0100

    add script and tomcat

---
+++ Automatisation Maven and deploy on tomcat.ps1
@@ -0,0 +1,14 @@
+﻿# Script pour automatiser Maven
+# version 1.0
+# Auteur : Flavien ANNAIX
+
+#Recuperation des sources
+mvn scm:checkout 
+
+
+#Pack le projet
+mvn package
+mvn clean install
+
+#deployer le war dans tomcat
+mvn tomcat7:deploy
